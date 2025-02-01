610657f147e50ca7a6312637b0e534c6d80256b9
Sleman37 <sleman0072000@gmail.com>
2025-02-01 11:31:50 +0200

    organized the divs as a 16x16 grid

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let divList = []
 let container = document.querySelector(".container")
 for (let i = 0; i < 256; i++) {
     let divSquare = document.createElement("div")
-    divSquare.style = "border: solid; height: 25px; width: 25px;"
+    divSquare.classList.add("square")
     divList.push(divSquare)
 }
 
