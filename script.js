25a9d929d12e3874ec58afeca6b585df06bc108c
Sleman37 <sleman0072000@gmail.com>
2025-02-02 23:13:17 +0200

    added button to create new grid
    added a buttons which is binded to an event listener
    which deletes the old grid and creates a brand new grid
    depending on the user's input of the size

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 let divList = []
 
+let btn = document.querySelector(".btn")
 let container = document.querySelector(".container")
 for (let i = 0; i < 256; i++) {
     let divSquare = document.createElement("div")
     divSquare.classList.add("square")
     divList.push(divSquare)
 }
+
+
+// event listener of button that removes current grid and creates new one
+btn.addEventListener("click", e => {
+    let newGrid = parseInt(prompt("Enter the size of your grid"))
+    if (typeof (newGrid) === "number") {
+        // let squares = document.querySelectorAll(".square")
+        // squares.forEach((square) => {
+        //     square.remove()
+        // })
+        if (newGrid > 100) {
+            alert("Please pick a number smaller than 100")
+        }
+        else {
+            let calculateWidth = 100 / (newGrid + 1)
+            container.innerHTML = '' //another way to remove all inner children
+            divList = []
+            for (let i = 0; i < newGrid * newGrid; i++) {
+                let divSquare = document.createElement("div")
+                divSquare.classList.add("square")
+                divSquare.style.flex = "1 0 " + calculateWidth + "%"
+                divList.push(divSquare)
+            }
+            divList.forEach(divSquare => {
+                divSquare.addEventListener("mouseover", e => {
+                    divSquare.classList.add("colorChange")
+                })
+            })
+            container.append(...divList)
+        }
+
+    }
+
+
+})
 
 divList.forEach(divSquare => {
     divSquare.addEventListener("mouseover", e => {
